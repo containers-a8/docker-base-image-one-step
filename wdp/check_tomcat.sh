196675c05888fd3c5d55f22b1e2b2102d6a5031b
manglu <manglu@gmail.com>
2016-11-23 09:57:36 +1100

    changed the health script to use awk instead of cut and it appears to work when executed as a script in the container.

--- wdp/check_tomcat.sh
+++ wdp/check_tomcat.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-TOMCAT_PATH="/usr/local/tomcat"
-#pid=$(ps x | grep "${TOMCAT_PATH}" | grep -v grep |cut -d ' ' -f 1)
-pid= $(/usr/bin/pgrep -f tomcat/bin/bootstrap)
+TOMCAT_PATH="/usr/local/tomcat/bin/bootstrap"
 
+pid=$(ps x | grep "${TOMCAT_PATH}" | grep -v grep | awk '{print $1}')
 echo "Tomcat PID is:" ${pid}
 
-if [ "${pid}" ]
-then
-  echo "Tomcat is online"
-  exit 0
-else
-  echo "Tomcat is offline"
-  exit 2
-fi
+
+                                                                                                                                                                       
+if [ "${pid}" ]                                                                                                                                                        
+then                                                                                                                                                                   
+  echo "Tomcat is online"                                                                                                                                              
+  exit 0                                                                                                                                                               
+else                                                                                                                                                                   
+  echo "Tomcat is offline"                                                                                                                                             
+  exit 2                                                                                                                                                               
+fi    
