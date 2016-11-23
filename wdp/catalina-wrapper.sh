4cb230a11ec3836e9c3bd3d71bfb81e0e7eac990
manglu <manglu@gmail.com>
2016-11-23 14:41:04 +1100

    updated wrapper with scripts for the deregister function

--- wdp/catalina-wrapper.sh
+++ wdp/catalina-wrapper.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Starting catalina-wrapper.sh"
-
-
 cleanup()
 {
   echo "Trapped signal. Cleaning up"
@@ -11,12 +8,29 @@ cleanup()
 
 deregister()
 {
-  echo "De-register service"
+  echo "About to de-register service-tomcat"
+#  curl -X PUT  http://localhost:8500/v1/agent/service/deregister/tomcat
+#  response=$(curl --write-out %{http_code} --silent --output /dev/null servername)
+
+   response=$(curl --write-out %{http_code} --silent --output /dev/null -X PUT  http://localhost:8500/v1/agent/service/deregister/tomcat)
+   
+   echo "Response code is:" ${response}
+   if [ "${response}" == '200' ]                                                                                                                                                        
+   then                                                                                                                                                                   
+     echo "Tomcat service is successfully de-registered"                                                                                                                                                                                                                                                                                                          
+   else                                                                                                                                                                   
+     echo "ERROR occured during service deregistration - response code returned is:"  ${response}                                                                                                                                                                                                                                                                                                      
+   fi   
+
 }
+
 
 register()
 {
   echo "Register service"
+  #  Start the consul agent
+  #  Extract the Consul host from the environment variable - $CONSUL_HOST
+  #consul agent -join $CONSUL_HOST -data-dir /data/consul -config-dir /etc/consul.d
 }
 
 trap cleanup SIGTERM SIGINT
